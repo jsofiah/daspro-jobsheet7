f980ba15bc8f213ffd7e39f05b9a1b586deb9991
sofiahdingdong <sofiahdingdong@gmail.com>
2024-10-17 08:54:18 +0700

    Percobaan 2 modif

--- SiakadWhile21.java
+++ SiakadWhile21.java
@@ -20,22 +20,22 @@ public class SiakadWhile21 {
                 System.out.println("Bagus, pertahankan nilainya");
             }
             else if (nilai > 73 && nilai <= 80){
-                System.out.println("Nilai mahasiswa ke" + (i+1) + " adalah B+");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B+");
             }
             else if(nilai > 65 && nilai <=73){
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B");
             }
             else if (nilai > 60 && nilai <= 65){
-                System.out.println("Nilai mahasiswa ke" + (i+1) + " adalah C+");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah C+");
             }
             else if (nilai > 50 && nilai <= 60){
-                System.out.println("Nilai mahasiswa ke" + (i+1) + " adalah C");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah C");
             }
             else if(nilai > 39 && nilai <=50){
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah D");
             }
             else{
-                System.out.println("Nilai mahasiswa ke" + (i+1) + " adalah E");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah E");
             }
             i++;
         }
